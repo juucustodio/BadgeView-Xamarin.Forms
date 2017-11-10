cbbe6af9deb5ba309c8c47fd0c6f794c3cd5212c
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-11-10 19:30:16 -0200

    Implement BadgeView for iOS application

---
+++ BadgeView/BadgeView.iOS/CircleBoxRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+using BadgeView.iOS;
+using BadgeView;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(CircleBox), typeof(CircleBoxRenderer))]
+namespace BadgeView.iOS
+{
+    public class CircleBoxRenderer : BoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element != null)
+            {
+                Layer.MasksToBounds = true;
+                UpdateCornerRadius(Element as CircleBox);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == CircleBox.CornerRadiusProperty.PropertyName)
+            {
+                UpdateCornerRadius(Element as CircleBox);
+            }
+        }
+
+        void UpdateCornerRadius(CircleBox box)
+        {
+            Layer.CornerRadius = (float)box.CornerRadius;
+        }
+    }
+}
